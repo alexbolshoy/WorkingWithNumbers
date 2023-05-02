0be97a31b4e18966dc64f307463968e5072dd6b4
Kalinin Alexander <kalina783@gmail.com>
2023-05-03 00:40:20 +0500

    Two independent classes and two conditions into one

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,17 @@ public class Main {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> positivList = new ArrayList<>();
         for (int list : intList) { //выбор всех значений больше нуля
-            if (list > 0) {
+            if (list > 0 && list % 2 == 0) {
                 positivList.add(list);
             }
         }
-            List<Integer> evenNemberList = new ArrayList<>();
-            for (int list : positivList) { // выбор всех четных чисел
-                if (list % 2 == 0) {
-                    evenNemberList.add(list);
-                }
-                }
-                evenNemberList.sort(Comparator.naturalOrder()); // сортировка по возрастанию
-                System.out.println("Работа с коллекцией");
-                System.out.println(evenNemberList);
 
-                System.out.println("Работа со стримами");
-                StreamMain streamMain = new StreamMain();
-                streamMain.start();
+        positivList.sort(Comparator.naturalOrder()); // сортировка по возрастанию
+                System.out.println("Работа с коллекцией");
+                System.out.println(positivList);
+
+//                System.out.println("Работа со стримами");
+//                StreamMain streamMain = new StreamMain();
+//                streamMain.start();
             }
         }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -4,6 +4,11 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Работа со стримами");
+        StreamMain streamMain = new StreamMain();
+        streamMain.start();
+    }
     public void start() {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         intList.stream()
